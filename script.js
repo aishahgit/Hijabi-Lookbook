705d188efd4323fb2c25d36d4ac5de170e498325
aishahgit <aishahsh789@gmail.com>
2024-11-07 10:20:33 -0800

    added script.js

--- script.js
+++ script.js
@@ -5,16 +5,3 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontent = document.getElementsByClassName("tab-content");
-
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontents of tabcontent){
-        tabcontents.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-} 
